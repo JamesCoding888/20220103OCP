87b514b814d76f553b9ed88b15442f3cb8c540a2
jamesliao <JamesCoding888@hotmail.com>
2022-03-28 22:12:21 +0800

    20220103ocp

--- ocp/src/com/study/ocp/day22/Test.java
+++ ocp/src/com/study/ocp/day22/Test.java
@@ -1,18 +1,22 @@
 package com.study.ocp.day22;
 
-interface Foo{
+interface Foo_1{
 	void eat();
 }
 public class Test {
 
 	public static void main(String[] args) {
-		Foo foo = new Foo() {
+		Foo_1 foo = new Foo_1() {
 			@Override
 			public void eat() {
 				System.out.println("eat");
 			}
+			public void play() {
+				System.out.println("play");
+			}
 		};
-		
+		foo.eat();
+		((Test$1)foo_1).play(); // JRE 1.5 可以執行，但之後就不行了
 		
 	}
 	
